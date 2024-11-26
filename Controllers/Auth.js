dc60c4bde738b22c9cd4fe274a5ef984721f2162
Tejas-A11 <161318927+Tejas-A11@users.noreply.github.com>
2024-11-26 11:38:34 +0530

    Update Auth.js

--- Controllers/Auth.js
+++ Controllers/Auth.js
@@ -4,6 +4,7 @@ const SellerModel = require("../Models/SellerModel");
 const bcrypt = require("bcryptjs");
 const generateToken = require("../Utils/generateToken");
 
+
 const buyerSignup = async (req, res) => {
   try {
     const { name, password, address, phone_num, email } = req.body;
